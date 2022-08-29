5937fa60968f6a6e3a447fdafaf8664062f8c4ca
NYATSIKOR Germaine <germaine.nyatsikor@agri-maker.com>
2022-08-29 17:10:31 +0200

    PageAccueilV1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'acf3ffe79649a76d0ca977b7c932deff',
+  movieApiUrl: 'https://api.themoviedb.org/3',
+  movieImgUrl:'https://image.tmdb.org/t/p/original',
 };
 
 /*
